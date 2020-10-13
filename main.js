e317ce050794be9727ef267c693c9f54210bd2ef
Alejandra Calderon <alejandracsanchez7@gmail.com>
2020-10-13 10:24:17 +0200

    Accept merge

--- main.js
+++ main.js
@@ -2,12 +2,15 @@
 let numberElement = document.querySelector(".js-type__number");
 let writeNumberElement = document.querySelector(".write-number");
 let attemptsElement = document.querySelector(".number__tried");
+let btnTry = document.querySelector(".form__button");
 
 function getNumberElement() {
   let getNumberElement = numberElement.value;
-  console.log(numberElement);
+  console.log(getnumberElement);
 }
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
+
+btnTry.addEventListener("click", getNumberElement);
