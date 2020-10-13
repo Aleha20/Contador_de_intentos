8406186ac4e6e01533c28eba4fe2ad6f3cb99dbd
Alejandra Calderon <alejandracsanchez7@gmail.com>
2020-10-13 11:04:57 +0200

    Failed ejercice

--- main.js
+++ main.js
@@ -4,14 +4,17 @@ let writeNumberElement = document.querySelector(".write-number");
 let attemptsElement = document.querySelector(".number__tried");
 let btnTry = document.querySelector(".form__button");
 
+// writeNumberElement.innerHTML = writeNumberElement.innerHTML + "Demasiado bajo";
+
 function randomNumber() {
   let randomNumber = Math.ceil(Math.random() * 100);
   console.log(Math.ceil(Math.random() * 100));
+  //   if (randomNumber < 1) {
+  //     writeNumberElement.classlist.add("write-number");
+  //   }
 }
 
-// function getRandomNumber(min, max) {
-//   return Math.ceil(Math.random() * 100);
-//   console.log(getRandomNumber);
-// }
-
 btnTry.addEventListener("click", randomNumber);
+
+//dejo comentado lo que sé que no funciona
+//He conseguido que me salga el
